cafbe5e7f9660f8a7785e8f9b18a17e449c65d54
abirmoy <abirmoy@163.com>
2021-01-01 02:10:14 +0600

    Changes made on Fri, Jan  1, 2021  2:10:14 AM

---
+++ shutdown_pc.py
@@ -0,0 +1,12 @@
+# FOR DETAILS OF PRARAMETER OPEN CMD PRMPT
+# TYPE>> shutdown /?
+
+import os
+import time
+
+# HIBERNATE IN 10s
+time.sleep(10)
+os.system('shutdown /h')
+
+# SHUTUDOWN IMMEDIATELY
+#os.system('shutdown /s')
